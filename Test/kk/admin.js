270f5ecbae6410f6822c67e75c674ecff6a662f8
xuanhei <2441415772@qq.com>
2020-09-21 09:47:14 +0800

    更新默认模板

--- Test/kk/admin.js
+++
@@ -1,7 +0,0 @@
-/**
- * Greated By xuanhei on 2020/9/5
- **/
-
-class Admin {
-
-}
--- Test/test.js
+++
@@ -1,5 +0,0 @@
-/**
- * Greated By xuanhei on 2020/9/5
- **/
-class admin{ sayHi(){} }
-console.log(admin.prototype.sayHi);
